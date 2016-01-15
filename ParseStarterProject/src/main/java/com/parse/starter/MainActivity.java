84a44df1a3f98cfe9fe5da3a937461aba311feda
sareth <ros.sareth.it@gmail.com>
2016-01-15 10:22:16 +0700

    Testing git by modify mainactivity

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -18,6 +18,9 @@ import com.parse.ParseAnalytics;
 
 public class MainActivity extends ActionBarActivity {
 
+
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
